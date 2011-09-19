6fee1d263a2da73ecc8718c13881d34cdd34d5be
Brennan Falkner <Brennan@bfalkner.com>
2011-09-19 07:08:34 -0400

    Rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DropostIt::Application.config.secret_token = 'eef845991a16da657b4c82eb8bbe222e6ecbb9d1c5db8957d1fecf4a8d5551944881ea3c9410b9a79704d8fe8ca882bf1e409a05bfc3a829d91ecc5eaf8124cb'
